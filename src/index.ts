5ecf2f888490801a40802bd560c884133b1c27c6
mohammadkadibalban <mohammad.kadibalban@endeavour.nl>
2026-05-28 18:11:12 +0200

    V5

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,13 @@ app.get("/omg", (req: Request, res: Response) => {
   });
 });
 
+app.get("/fifi", (req: Request, res: Response) => {
+  res.json({
+    message: "This is version 2 API!",
+    version: "5.0.0"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`test-app is running on port ${PORT}`);
 });
